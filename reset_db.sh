0ebcded91836c6ad754b54305f4bccb89c796afc
k21971 <keith.simpson1971@gmail.com>
2022-10-08 13:43:17 +0000

    Tweak reset_db script.

--- reset_db.sh
+++ reset_db.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
-# DO NOT run this script during an active tournament
+# DO NOT run this script during an active tournament. This will
+# perform a full reset of the scoreboard database.
 
-# This does NOT do a full reset for next year's tournament. It is for when a
-# model has just been changed and you want to recreate the database with that
-# new structure.
 . secrets.sh
-# Uncommment the below line if a full reset needs to be performed
-#./manage.py wipe_db --all
+./manage.py wipe_db --all
 ./manage.py migrate scoreboard zero && rm -r scoreboard/migrations/* && \
     ./manage.py makemigrations scoreboard && ./manage.py migrate scoreboard
