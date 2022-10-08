53f62f20bc0974ce96cc85084aecd57c47527663
k21971 <keith.simpson1971@gmail.com>
2022-10-08 13:26:19 +0000

    Include command to perform a full database reset.
    Commented out by default. Thanks aosdict.

--- reset_db.sh
+++ reset_db.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
+# DO NOT run this script during an active tournament
+
 # This does NOT do a full reset for next year's tournament. It is for when a
 # model has just been changed and you want to recreate the database with that
 # new structure.
 . secrets.sh
+# Uncommment the below line if a full reset needs to be performed
+#./manage.py wipe_db --all
 ./manage.py migrate scoreboard zero && rm -r scoreboard/migrations/* && \
     ./manage.py makemigrations scoreboard && ./manage.py migrate scoreboard
